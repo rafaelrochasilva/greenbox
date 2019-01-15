ea5c5a05cf027b65ae1ffc5809cb20c6625edee8
Rafael Rocha <rafael.rocha@therealreal.com.br>
2019-01-15 14:38:34 -0200

    Remove unecessary code

--- lib/greenbox.ex
+++ lib/greenbox.ex
@@ -1,18 +1,2 @@
 defmodule Greenbox do
-  @moduledoc """
-  Documentation for Greenbox.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Greenbox.hello()
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end
--- test/greenbox_test.exs
+++ test/greenbox_test.exs
@@ -1,8 +1,4 @@
 defmodule GreenboxTest do
   use ExUnit.Case
   doctest Greenbox
-
-  test "greets the world" do
-    assert Greenbox.hello() == :world
-  end
 end
