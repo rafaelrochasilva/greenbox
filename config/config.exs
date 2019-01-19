79955b73f8f98244dad28e4fa22b90d93a773881
Rafael Rocha <rafael.rocha@therealreal.com.br>
2019-01-19 12:18:59 -0200

    Add lib to make external request

--- config/config.exs
+++ config/config.exs
@@ -1,6 +1,10 @@
 use Mix.Config
 
-# Uses the FakeClient also for dev and prod since this abc_products_client does
-# not exists.
+# Uses the FakeClient also for dev and prod since this abc_products_client
+# does not exists.
 config :greenbox,
   abc_products_client: Greenbox.FakeClient
+# config :greenbox,
+#   abc_products_client: Greenbox.ProductClient,
+#   abc_products_url: "http://abcproducts.com/products"
+
--- lib/product_client.ex
+++ lib/product_client.ex
@@ -1,6 +1,10 @@
 defmodule Greenbox.ProductClient do
-
   def fetch_products do
-    # HTTPoison.get! "http://localhost:1"
+    response = url() |> HTTPoison.get!()
+    Poison.decode!(response.body)
+  end
+
+  defp url do
+    Application.get_env(:greenbox, :abc_products_url)
   end
 end
